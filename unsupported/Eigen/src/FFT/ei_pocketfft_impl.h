aa6475c32f35ed0e2f8d640ae9935ad48c62b171
guoqiangqi <425418567@qq.com>
2021-01-08 14:56:23 +0800

    Modify some local variables

--- unsupported/Eigen/src/FFT/ei_pocketfft_impl.h
+++ unsupported/Eigen/src/FFT/ei_pocketfft_impl.h
@@ -13,32 +13,33 @@ struct pocket_fft_impl
   typedef std::complex<Scalar> Complex;
 
   inline void fwd(Complex* dst, const Complex* src, int nfft){
-
     const shape_t  shape_{ static_cast<size_t>(nfft) };
     const shape_t  axes_{ 0 };
-    const stride_t stride_{ 1 };
-    c2c(shape_, stride_, stride_, axes_, FORWARD, src, dst, static_cast<Scalar>(nfft));
+    const stride_t stride_{ sizeof(Complex) };
+    c2c(shape_, stride_, stride_, axes_, FORWARD, src, dst, static_cast<Scalar>(1));
   }
 
   inline void fwd(Complex* dst, const Scalar* src, int nfft){
     const shape_t  shape_{ static_cast<size_t>(nfft) };
     const shape_t  axes_{ 0 };
-    const stride_t stride_{ 1 };
-    r2c(shape_, stride_, stride_, axes_, FORWARD, src, dst, static_cast<Scalar>(nfft));
+    const stride_t stride_in{ sizeof(Scalar) };
+    const stride_t stride_out{ sizeof(Complex) };
+    r2c(shape_, stride_in, stride_out, axes_, FORWARD, src, dst, static_cast<Scalar>(1));
   }
 
   inline void inv(Complex* dst, const Complex* src, int nfft){
     const shape_t  shape_{ static_cast<size_t>(nfft) };
     const shape_t  axes_{ 0 };
-    const stride_t stride_{ 1 };
-    c2c(shape_, stride_, stride_, axes_, BACKWARD, src, dst, static_cast<Scalar>(nfft));
+    const stride_t stride_{ sizeof(Complex) };
+    c2c(shape_, stride_, stride_, axes_, BACKWARD, src, dst, static_cast<Scalar>(1));
   }
 
   inline void inv(Scalar* dst,  const Complex* src, int nfft){
     const shape_t  shape_{ static_cast<size_t>(nfft) };
     const shape_t  axes_{ 0 };
-    const stride_t stride_{ 1 };
-    c2r(shape_, stride_, stride_, axes_, BACKWARD, src, dst, static_cast<Scalar>(nfft));
+    const stride_t stride_in{ sizeof(Complex) };
+    const stride_t stride_out{ sizeof(Scalar) };
+    c2r(shape_, stride_in, stride_out, axes_, BACKWARD, src, dst, static_cast<Scalar>(1));
   }
 
 };
