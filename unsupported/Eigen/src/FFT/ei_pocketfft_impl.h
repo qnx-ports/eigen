d795df95ab09f94acb3f8c0ae37e5b41fe83f52d
guoqiangqi <425418567@qq.com>
2021-01-08 11:20:13 +0800

    Using pocketfft impl as eigen default fft impl

--- unsupported/Eigen/src/FFT/ei_pocketfft_impl.h
+++ unsupported/Eigen/src/FFT/ei_pocketfft_impl.h
@@ -12,14 +12,36 @@ struct pocket_fft_impl
   typedef _Scalar Scalar;
   typedef std::complex<Scalar> Complex;
 
-  inline void fwd(Complex* dst, Complex* src, int nfft){
+  inline void fwd(Complex* dst, const Complex* src, int nfft){
 
+    const shape_t  shape_{ static_cast<size_t>(nfft) };
+    const shape_t  axes_{ 0 };
+    const stride_t stride_{ 1 };
+    c2c(shape_, stride_, stride_, axes_, FORWARD, src, dst, static_cast<Scalar>(nfft));
   }
 
-  inline void fwd(Complex* dst, Scalar* src, int nfft){
-    
+  inline void fwd(Complex* dst, const Scalar* src, int nfft){
+    const shape_t  shape_{ static_cast<size_t>(nfft) };
+    const shape_t  axes_{ 0 };
+    const stride_t stride_{ 1 };
+    r2c(shape_, stride_, stride_, axes_, FORWARD, src, dst, static_cast<Scalar>(nfft));
   }
-}
+
+  inline void inv(Complex* dst, const Complex* src, int nfft){
+    const shape_t  shape_{ static_cast<size_t>(nfft) };
+    const shape_t  axes_{ 0 };
+    const stride_t stride_{ 1 };
+    c2c(shape_, stride_, stride_, axes_, BACKWARD, src, dst, static_cast<Scalar>(nfft));
+  }
+
+  inline void inv(Scalar* dst,  const Complex* src, int nfft){
+    const shape_t  shape_{ static_cast<size_t>(nfft) };
+    const shape_t  axes_{ 0 };
+    const stride_t stride_{ 1 };
+    c2r(shape_, stride_, stride_, axes_, BACKWARD, src, dst, static_cast<Scalar>(nfft));
+  }
+
+};
 
 } // namespace internal
 } // namespace Eigen
