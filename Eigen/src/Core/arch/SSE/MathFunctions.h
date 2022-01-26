2a93f5a7617884ed19e5bda72efc6310d7813572
Rasmus Munk Larsen <rmlarsen@google.com>
2022-01-26 11:13:32 -0800

    Make preciprocal IEEE compliant w.r.t. 1/0 and 1/inf.

--- Eigen/src/Core/arch/SSE/MathFunctions.h
+++ Eigen/src/Core/arch/SSE/MathFunctions.h
@@ -148,11 +148,15 @@ Packet4f prsqrt<Packet4f>(const Packet4f& _x) {
   return pselect<Packet4f>(not_normal_finite_mask, y_approx, y_newton);
 }
 
-#endif
-
+#ifdef EIGEN_VECTORIZE_FMA
+// Without FMA it is faster to just use the division instruction.
 template<> EIGEN_STRONG_INLINE Packet4f preciprocal<Packet4f>(const Packet4f& a) {
   return generic_reciprocal_newton_step<Packet4f, /*Steps=*/1>::run(a, _mm_rcp_ps(a));
 }
+#endif
+
+#endif
+
 
 
 // Hyperbolic Tangent function.
