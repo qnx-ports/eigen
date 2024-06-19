2e4d9854ab49c995888df24fb063ae584848e253
Charlie Schlosser <cs.schlosser@gmail.com>
2024-06-19 13:25:40 -0400

    fix unused variable warning

--- Eigen/src/SVD/BDCSVD.h
+++ Eigen/src/SVD/BDCSVD.h
@@ -52,7 +52,7 @@ struct traits<BDCSVD<MatrixType_, Options> > : svd_traits<MatrixType_, Options> 
 
 template <typename MatrixType, int Options>
 struct allocate_small_svd {
-  static void run(JacobiSVD<MatrixType, Options>& smallSvd, Index rows, Index cols, unsigned int computationOptions) {
+  static void run(JacobiSVD<MatrixType, Options>& smallSvd, Index rows, Index cols, unsigned int /*computationOptions*/) {
     internal::construct_at(&smallSvd, rows, cols);
   }
 };
@@ -63,7 +63,7 @@ EIGEN_DISABLE_DEPRECATED_WARNING
 template <typename MatrixType>
 struct allocate_small_svd<MatrixType, 0> {
   static void run(JacobiSVD<MatrixType>& smallSvd, Index rows, Index cols, unsigned int computationOptions) {
-    smallSvd = JacobiSVD<MatrixType>(rows, cols, computationOptions);
+    internal::construct_at(&smallSvd, rows, cols, computationOptions);
   }
 };
 
